61aa3c25235e460880dd5645137936a37534364a
Jmilicev <jovan.milicev@gmail.com>
2021-05-12 21:56:12 -0400

    rewrote with ejs

--- app.js
+++ app.js
@@ -1,28 +1,78 @@
 
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
 const express = require('express')
-const app = express()
+const flash = require('express-flash');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const passport = require('passport')
+
+const app = express();
 const path = require('path');
-const port = 3000
+const port = 3000;
+
+app.set('view-engine', 'ejs');
+
+const initializePassport = require('./passport-config');
+
+initializePassport(
+    passport, 
+    email => users.find(user => user.email === email)
+)
+
+
+const users = [];
+const password = [];
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(express.static('source'));
+//app.use(express.static('source'));
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+//app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/source/home.html'));
+    res.render('index.ejs', { name : "Jovan"});
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname+'/source/login.html'));
+    res.render('login.ejs');
 })
 
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname+'/source/home.html'));
+app.get('/register', (req, res) => {
+    res.render('register.ejs');
 })
 
-app.get('/ttt', (req, res) => {
-    res.sendFile(path.join(__dirname+'/source/tictactoe.html'));
+app.post('/register', async (req, res) => {
+    try{
+        const hasedpswd = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name : req.body.name,
+            email: req.body.email,
+            password: hasedpswd,
+        })
+        res.redirect('/login');
+    }
+    catch{
+        res.redirect('/register');
+    }
 })
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`app.js listening on http://0.0.0.0:${port}`)
